f2e43ec138865b4ebec734243e71fb7c8f7dc5dd
Stephen Williams <19681224+stephenindia1@users.noreply.github.com>
2019-04-17 12:24:11 +0300

    Update dbconn.php

--- dbconn.php
+++ dbconn.php
@@ -1,10 +1,9 @@
 <?php
 
-
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'abcd');
-define('DB_NAME', 'abcd');
+define('DB_USERNAME', 'stephenindia_dba');
+define('DB_PASSWORD', 'Exodus7$');
+define('DB_NAME', 'webdev');
  
 /* Attempt to connect to the database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
